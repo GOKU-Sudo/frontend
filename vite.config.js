436b59e22f072afbae23b2b39f99006af247911a
RUDRA MADHAB PRADHAN <rudramadhabpradhanx1000@gmail.com>
2024-05-02 22:26:32 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "https://owner-avatar-mern-ecommerce-backend.vercel.app/",
+      "/uploads/": "https://owner-avatar-mern-ecommerce-backend.vercel.app/",
     },
   },
 });
